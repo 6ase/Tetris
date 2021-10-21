2c6c5943235f102f690665c2fb9dbdd0462b8641
6ase <Serghome06@mail.ru>
2021-10-21 22:51:29 +0300

    Реализовал создание рандомной фигуры и сделал её падение

---
+++ Tetris/Point.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Point
+    {
+        public Point(int a, int b, char sym) { x = a; y = b; c = sym; }
+        public Point() { }
+
+        public int x;
+        public int y;
+        public char c;
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+        
+        internal void Move(Direction dir)
+        {
+            switch (dir) 
+            { 
+                case Direction.DOWN: y += 1; break;
+                case Direction.LEFT: x += -1; break;
+                case Direction.RIGHT: x += 1; break;
+            }
+        }
+       
+        internal void Hide()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(" ");
+        }
+    }
+}
--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Tetris
@@ -10,7 +11,26 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            FigureGenerator generator = new FigureGenerator(20, 0, '*');
+            Figure s = null;
+
+            while (true)
+            {
+                s = generator.GetNewFigure();
+                for(int i = 0; i < 15; i++)
+                {
+                    s.Draw();
+                    Thread.Sleep(200);
+                    s.Hide();
+                    s.Move(Direction.DOWN);
+                }
+                
+            }
+
+            
         }
     }
 }
